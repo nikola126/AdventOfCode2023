7ddc3d1953d0de74d6df7507ab8eed272e935078
Nikola <nikj1705@gmail.com>
2023-12-08 20:31:26 +0200

    Day 8 Part 2 complete

--- src/main/java/com/adventofcode2023/Day8.java
+++ src/main/java/com/adventofcode2023/Day8.java
@@ -16,6 +16,7 @@ public class Day8 {
     public Day8() {
         try {
             partOne();
+            partTwo();
         } catch (Exception e) {
             System.out.println("Error with Day 8: " + e.getMessage());
         }
@@ -45,7 +46,7 @@ public class Day8 {
                 String left = directions[0].substring(1).trim();
                 String right = directions[1].substring(0, directions[1].length() - 1);
 
-                map.put(node, new Node(node, left, right, 0));
+                map.put(node, new Node(node, left, right));
             } else {
                 for (Character c : line.toCharArray())
                     instructions.add(c);
@@ -56,7 +57,6 @@ public class Day8 {
                 .filter(node -> node.getValue().getText().equals("AAA"))
                 .findFirst().get().getValue();
 
-        currentNode.incrementVisited();
         int index = 0;
         int steps = 0;
 
@@ -70,12 +70,109 @@ public class Day8 {
                 break;
 
             steps += 1;
-            currentNode.incrementVisited();
 
             currentNode = map.get(goLeft ? currentNode.getLeft() : currentNode.getRight());
         }
 
         System.out.println("Day 8 Part 1: " + steps);
+    }
+
+    public void partTwo() throws FileNotFoundException {
+        scanner = new Scanner(file);
+
+        List<Character> instructions = new ArrayList<>();
+        Map<String, Node> map = new LinkedHashMap<>();
+
+        boolean inMap = false;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+
+            if (line.isEmpty()) {
+                inMap = true;
+                continue;
+            }
+
+            if (inMap) {
+                String[] tokens = line.split(" = ");
+                String node = tokens[0].trim();
+                String[] directions = tokens[1].split(", ");
+
+                String left = directions[0].substring(1).trim();
+                String right = directions[1].substring(0, directions[1].length() - 1);
+
+                map.put(node, new Node(node, left, right));
+            } else {
+                for (Character c : line.toCharArray())
+                    instructions.add(c);
+            }
+        }
+
+        List<Node> startingNodes = map.values().stream()
+                .filter(node -> node.getText().endsWith("A"))
+                .toList();
+
+        List<Long> stepsUntilLoop = new ArrayList<>();
+        for (Node node : startingNodes) {
+            int index = 0;
+            long steps = 0;
+
+            Node currentNode = node;
+
+            while (true) {
+                if (index == instructions.size())
+                    index = 0;
+
+                boolean goLeft = instructions.get(index) == 'L';
+
+                currentNode = map.get(goLeft ? currentNode.getLeft() : currentNode.getRight());
+                steps++;
+                index++;
+
+                if (currentNode.getText().endsWith("Z")) {
+                    stepsUntilLoop.add(steps);
+                    break;
+                }
+            }
+        }
+
+        System.out.println("Day 8 Part 2: " + LCM(stepsUntilLoop));
+    }
+
+    // https://stackoverflow.com/a/4202114
+    public static long GCD(long a, long b) {
+        while (b > 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+
+    // https://stackoverflow.com/a/4202114
+    public static long GCD(List<Long> inputs) {
+        long result = inputs.get(0);
+
+        for (int i = 1; i < inputs.size(); i++)
+            result = GCD(result, inputs.get(i));
+
+        return result;
+    }
+
+    // https://stackoverflow.com/a/4202114
+    public static long LCM(long a, long b) {
+        return a * (b / GCD(a, b));
+    }
+
+    // https://stackoverflow.com/a/4202114
+    public static long LCM(List<Long> inputs) {
+        long result = inputs.get(0);
+
+        for (int i = 1; i < inputs.size(); i++)
+            result = LCM(result, inputs.get(i));
+
+        return result;
     }
 
     @AllArgsConstructor
@@ -85,15 +182,23 @@ public class Day8 {
         private String text;
         private String left;
         private String right;
-        private int countVisited;
 
         @Override
         public String toString() {
-            return String.format("[%s] (%s) (%s) [%d]", text, left, right, countVisited);
+            return String.format("[%s] (%s) (%s)", text, left, right);
         }
 
-        public void incrementVisited() {
-            this.countVisited += 1;
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Node node = (Node) o;
+            return Objects.equals(text, node.text);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(text);
         }
     }
 
