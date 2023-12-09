424f13072f63712427b96938ba2207c1b825af3f
Nikola126 <nikj1705@gmail.com>
2023-12-09 11:08:29 +0200

    Day 9 Part 2 complete

--- src/main/java/com/adventofcode2023/Day9.java
+++ src/main/java/com/adventofcode2023/Day9.java
@@ -15,6 +15,7 @@ public class Day9 {
     public Day9() {
         try {
             partOne();
+            partTwo();
         } catch (Exception e) {
             System.out.println("Error with Day 9: " + e.getMessage());
         }
@@ -35,17 +36,22 @@ public class Day9 {
 
             Stack<List<Integer>> stack = new Stack<>();
             stack.push(history);
-            boolean allZeroes = false;
 
-            while (!allZeroes) {
+            while (true) {
                 List<Integer> current = stack.peek();
                 List<Integer> next = new ArrayList<>();
 
-                for (int i = 0; i < current.size() - 1; i++)
-                    next.add(current.get(i + 1) - current.get(i));
+                boolean nonZeroWasAdded = false;
 
-                allZeroes = next.stream().allMatch(number -> number == 0);
+                for (int i = 0; i < current.size() - 1; i++) {
+                    next.add(current.get(i + 1) - current.get(i));
+                    nonZeroWasAdded = next.getLast() != 0;
+                }
+
                 stack.push(next);
+
+                if (!nonZeroWasAdded)
+                    break;
             }
 
             stack.pop();
@@ -59,6 +65,52 @@ public class Day9 {
         }
 
         System.out.println("Day 9 Part 1: " + answer);
+    }
+
+    public void partTwo() throws FileNotFoundException {
+        scanner = new Scanner(file);
+
+        int answer = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            List<Integer> history = new ArrayList<>();
+
+            String[] tokens = line.split(" ");
+            for (String token : tokens)
+                history.add(Integer.parseInt(token.trim()));
+
+            Stack<List<Integer>> stack = new Stack<>();
+            stack.push(history);
+
+            while (true) {
+                List<Integer> current = stack.peek();
+                List<Integer> next = new ArrayList<>();
+
+                boolean nonZeroWasAdded = false;
+
+                for (int i = 0; i < current.size() - 1; i++) {
+                    next.add(current.get(i + 1) - current.get(i));
+                    nonZeroWasAdded = next.getLast() != 0;
+                }
+
+                stack.push(next);
+
+                if (!nonZeroWasAdded)
+                    break;
+            }
+
+            stack.pop();
+            int lastOnTheLeft = 0;
+            while (!stack.isEmpty()) {
+                List<Integer> list = stack.pop();
+                lastOnTheLeft = list.getFirst() - lastOnTheLeft;
+            }
+
+            answer += lastOnTheLeft;
+        }
+
+        System.out.println("Day 9 Part 2: " + answer);
     }
 
 }
