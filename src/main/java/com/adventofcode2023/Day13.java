f7722ab3ad79f80b6e9178b4f6233b7ac2d6592c
Nikola <nikj1705@gmail.com>
2023-12-13 22:29:16 +0200

    Day 13 Part 2 Complete

--- src/main/java/com/adventofcode2023/Day13.java
+++ src/main/java/com/adventofcode2023/Day13.java
@@ -14,6 +14,7 @@ public class Day13 {
     public Day13() {
         try {
             partOne();
+            partTwo();
         } catch (Exception e) {
             System.out.println("Error with Day 13: " + e.getMessage());
         }
@@ -33,6 +34,31 @@ public class Day13 {
         }
 
         System.out.println("Day 13 Part 1: " + answer);
+    }
+
+    private boolean smudgeFixed = false;
+
+    public void partTwo() throws FileNotFoundException {
+        scanner = new Scanner(file);
+
+        List<List<String>> grids = readGrids(scanner);
+
+        int answer = 0;
+        for (List<String> grid : grids) {
+            smudgeFixed = false;
+            int rowOfReflection = 0;
+            int colOfReflection = 0;
+
+            rowOfReflection = determineRowOfReflectionWithSmudges(grid);
+
+            if (!smudgeFixed || rowOfReflection == 0)
+                colOfReflection = determineRowOfReflectionWithSmudges(transpose(grid));
+
+            int sum = colOfReflection + 100 * rowOfReflection;
+            answer += sum;
+        }
+
+        System.out.println("Day 13 Part 2: " + answer);
     }
 
     private void printGrid(List<String> grid) {
@@ -90,12 +116,12 @@ public class Day13 {
             String nextLine = grid.get(row + 1);
 
             if (line.equals(nextLine)) {
-                int maxReflectionLength = Math.min(row, ROWS - (row + 1) - 1);
+                int maxReflectionLength = Math.min(row, ROWS - 1 - (row + 1));
 
                 boolean maxReflectionReached = true;
-                for (int refl = 0; refl < maxReflectionLength; refl++) {
-                    String lineOver = grid.get(row - refl - 1);
-                    String lineBelow = grid.get(row + refl + 2);
+                for (int ref = 0; ref < maxReflectionLength; ref++) {
+                    String lineOver = grid.get(row - ref - 1);
+                    String lineBelow = grid.get(row + ref + 2);
 
                     if (!lineOver.equals(lineBelow)) {
                         maxReflectionReached = false;
@@ -106,10 +132,94 @@ public class Day13 {
                 if (maxReflectionReached) {
                     return row + 1;
                 }
-
             }
         }
         return 0;
+    }
+
+    private int determineRowOfReflectionWithSmudges(List<String> grid) {
+        int ROWS = grid.size();
+
+        int smudgeReplacementIndex = 0;
+        String smudgeReplacementLine = "";
+
+        for (int row = 0; row < ROWS - 1; row++) {
+            String line = grid.get(row);
+            String nextLine = grid.get(row + 1);
+
+            int differences = countDifferences(line, nextLine);
+
+            if (differences == 1 && smudgeFixed)
+                return 0; // cannot fix another smudge and lines are not equal, so no reflection
+
+            if (line.equals(nextLine) || differences == 1) {
+                if (differences == 1) {
+                    // remember where the candidate smudge is and keep looking forward
+                    smudgeReplacementIndex = row;
+                    smudgeReplacementLine = nextLine;
+                    smudgeFixed = true;
+                }
+
+                int maxReflectionLength = Math.min(row, ROWS - 1 - (row + 1));
+
+                boolean maxReflectionReached = true;
+
+                for (int ref = 0; ref < maxReflectionLength; ref++) {
+                    String lineOver = grid.get(row - ref - 1);
+                    String lineBelow = grid.get(row + ref + 2);
+
+                    if (lineOver.equals(lineBelow))
+                        continue;
+
+                    if (countDifferences(lineOver, lineBelow) == 1) {
+                        if (smudgeFixed) {
+                            // cannot fix another smudge, lines are not equal and edge was also not reached
+                            maxReflectionReached = false;
+                            break;
+                        } else {
+                            // remember where the candidate smudge is and keep looking forward
+                            smudgeReplacementIndex = row - ref - 1;
+                            smudgeReplacementLine = lineBelow;
+
+                            smudgeFixed = true;
+                            continue;
+                        }
+                    }
+
+                    if (countDifferences(lineOver, lineBelow) != 1) {
+                        // more than one difference, it's not a smudge, so it's not a reflection
+                        maxReflectionReached = false;
+                        break;
+                    }
+                }
+
+                if (!smudgeFixed) {
+                    // maybe the reflection is in another row, keep looking
+                }
+                else if (maxReflectionReached) {
+                    // smudge was fixed, CHANGE THE GRID!
+                    // and return (add one because the row itself contributes to the length of the reflection)
+                    grid.set(smudgeReplacementIndex, smudgeReplacementLine);
+                    return row + 1;
+                }
+                else {
+                    // the smudge was fixed, but the edge was not reached
+                    // ignore the last candidate smudge and start over
+                    smudgeFixed = false;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int countDifferences(String a, String b) {
+        int diffs = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i))
+                diffs += 1;
+        }
+
+        return diffs;
     }
 
 }
