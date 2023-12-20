2399d55e85aa30a0a01b588417becdbb4a7a0e46
Nikola126 <nikj1705@gmail.com>
2023-12-21 00:49:52 +0200

    Day 19 Part 2 Complete

--- src/main/java/com/adventofcode2023/Day19.java
+++ src/main/java/com/adventofcode2023/Day19.java
@@ -15,6 +15,7 @@ public class Day19 {
     public Day19() {
         try {
             partOne();
+            partTwo();
         } catch (Exception e) {
             System.out.println("Error with Day 19: " + e.getMessage());
         }
@@ -45,6 +46,147 @@ public class Day19 {
         }
 
         System.out.println("Day 19 Part 1: " + sum);
+    }
+
+    private long combinations = 0;
+
+    public void partTwo() throws Exception {
+        scanner = new Scanner(file);
+
+        List<Workflow> workflows = new ArrayList<>();
+        List<Part> parts = new ArrayList<>();
+
+        readInput(scanner, workflows, parts);
+
+        Map<String, Workflow> workflowMap = new LinkedHashMap<>();
+        for (Workflow workflow : workflows)
+            workflowMap.put(workflow.getName(), workflow);
+
+        // Start with a range of possible XMAS values
+        Range range = new Range(1, 4000, 1, 4000, 1, 4000, 1, 4000);
+
+        // Move the full range through the starting workflow
+        moveRangeIntoRule(range, "in", workflowMap);
+
+        System.out.println("Day 19 Part 2: " + combinations);
+    }
+
+    private void moveRangeIntoRule(Range range, String workflowName, Map<String, Workflow> workflowMap) {
+        Workflow workflow = workflowMap.getOrDefault(workflowName, null);
+
+        if (workflow == null) {
+            // this happens on A or R
+            if (workflowName.equals("A")) {
+                // increment the global combination count with all possible combinations reached this far
+                combinations += range.getCombinations();
+            }
+            return;
+        }
+
+        for (String rule : workflow.getRules()) {
+            if (rule.equals("A")) {
+                // increment the global combination count with all possible combinations reached this far
+                combinations += range.getCombinations();
+            }
+            else if (rule.equals("R"))
+                return; // all rejected, do nothing
+            else if (rule.contains("<")) {
+                String category = rule.split("<")[0].trim();
+                String val = rule.split("<")[1].split(":")[0].trim();
+                String out = rule.split(":")[1].trim();
+
+                int value = Integer.parseInt(val);
+
+                Range nextRange = null;
+                // Construct a new range, with the specific parameter changed, based on the new limit
+                // Also "mutate" the old range, so that only the "reverse" limit is valid
+                switch (category) {
+                    case "x" -> {
+                        nextRange = new Range(
+                                range.getMinX(), value - 1,
+                                range.getMinM(), range.getMaxM(),
+                                range.getMinA(), range.getMaxA(),
+                                range.getMinS(), range.getMaxS());
+                        range.setMinX(value);
+                    }
+                    case "m" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                range.getMinM(), value - 1,
+                                range.getMinA(), range.getMaxA(),
+                                range.getMinS(), range.getMaxS());
+                        range.setMinM(value);
+                    }
+                    case "a" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                range.getMinM(), range.getMaxM(),
+                                range.getMinA(), value - 1,
+                                range.getMinS(), range.getMaxS());
+                        range.setMinA(value);
+                    }
+                    case "s" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                range.getMinM(), range.getMaxM(),
+                                range.getMinA(), range.getMaxA(),
+                                range.getMinS(), value - 1);
+                        range.setMinS(value);
+                    }
+                }
+
+                // Then, move the mutated range in the next workflow
+                moveRangeIntoRule(nextRange, out, workflowMap);
+            } else if (rule.contains(">")) {
+                String category = rule.split(">")[0].trim();
+                String val = rule.split(">")[1].split(":")[0].trim();
+                String out = rule.split(":")[1].trim();
+
+                int value = Integer.parseInt(val);
+
+                Range nextRange = null;
+                // Same as above, but in different direction
+                switch (category) {
+                    case "x" -> {
+                        nextRange = new Range(
+                                value + 1, range.getMaxX(),
+                                range.getMinM(), range.getMaxM(),
+                                range.getMinA(), range.getMaxA(),
+                                range.getMinS(), range.getMaxS());
+                        range.setMaxX(value);
+                    }
+                    case "m" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                value + 1, range.getMaxM(),
+                                range.getMinA(), range.getMaxA(),
+                                range.getMinS(), range.getMaxS());
+                        range.setMaxM(value);
+                    }
+                    case "a" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                range.getMinM(), range.getMaxM(),
+                                value + 1, range.getMaxA(),
+                                range.getMinS(), range.getMaxS());
+                        range.setMaxA(value);
+                    }
+                    case "s" -> {
+                        nextRange = new Range(
+                                range.getMinX(), range.getMaxX(),
+                                range.getMinM(), range.getMaxM(),
+                                range.getMinA(), range.getMaxA(),
+                                value + 1, range.getMaxS());
+                        range.setMaxS(value);
+                    }
+                }
+
+                moveRangeIntoRule(nextRange, out, workflowMap);
+            } else {
+                // It's not A, R, and it's not a rule, so just move the mutated range to the next workflow
+                moveRangeIntoRule(range, rule, workflowMap);
+            }
+        }
     }
 
     private void readInput(Scanner scanner, List<Workflow> workflows, List<Part> parts) {
@@ -181,6 +323,40 @@ public class Day19 {
         @Override
         public String toString() {
             return String.format("Workflow [%s] with [%d] rules", name, rules.size());
+        }
+    }
+
+    @AllArgsConstructor
+    @Getter
+    @Setter
+    private class Range {
+        int minX;
+        int maxX;
+        int minM;
+        int maxM;
+        int minA;
+        int maxA;
+        int minS;
+        int maxS;
+
+        @Override
+        public String toString() {
+            return String.format("RANGE X[%d:%d] M[%d:%d] A[%d:%d] S[%d:%d] [%d]",
+                    minX, maxX,
+                    minM, maxM,
+                    minA, maxA,
+                    minS, maxS,
+                    getCombinations());
+        }
+
+        public long getCombinations() {
+            long sum = 1;
+            sum *= ((long) maxX - (long) minX + 1L);
+            sum *= ((long) maxM - (long) minM + 1L);
+            sum *= ((long) maxA - (long) minA + 1L);
+            sum *= ((long) maxS - (long) minS + 1L);
+
+            return sum;
         }
     }
 
